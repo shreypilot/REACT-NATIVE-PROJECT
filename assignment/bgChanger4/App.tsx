3f8fae780aeb6fedb5780ad9b528c6dcad6719be
shreya kumar <shreypilot28@gmail.com>
2023-04-19 18:49:11 +0530

    make bgchngr project

--- assignment/bgChanger4/App.tsx
+++ assignment/bgChanger4/App.tsx
@@ -5,113 +5,142 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState} from 'react';
+
 import {
-  SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [randomColors, setRandomColors] = useState({
+    randomBackground: '#ffffff',
+    randomBoxColor1: '#EF5354',
+    randomBoxColor2: '#50DBB4',
+  });
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const generateColor = () => {
+    const hexRange = '0123456789ABCDEF';
+    let color = '#';
+
+    for (let i = 0; i < 6; i++) {
+      color += hexRange[Math.floor(Math.random() * 16)];
+    }
+
+    let randomBoxColor1 = '#';
+
+    for (let i = 0; i < 6; i++) {
+      randomBoxColor1 += hexRange[Math.floor(Math.random() * 16)];
+    }
+
+    // const randomBoxColor1 =
+    //   '#' +
+    //   hexRange[Math.floor(Math.random() * 16)] +
+    //   hexRange[Math.floor(Math.random() * 16)] +
+    //   hexRange[Math.floor(Math.random() * 16)];//this code only  but only uses 3 characters (representing RGB values) to
+    //create a 3-digit hexadecimal color code
+
+    let randomBoxColor2 = '#';
+
+    for (let i = 0; i < 6; i++) {
+      randomBoxColor2 += hexRange[Math.floor(Math.random() * 16)];
+    }
+
+    // const randomBoxColor2 =
+    //   '#' +
+    //   hexRange[Math.floor(Math.random() * 16)] +
+    //   hexRange[Math.floor(Math.random() * 16)] +
+    //   hexRange[Math.floor(Math.random() * 16)];
+
+    setRandomColors({
+      randomBackground: color,
+      randomBoxColor1,
+      randomBoxColor2,
+    });
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <>
+      <StatusBar backgroundColor={randomColors.randomBackground} />
+      <View
+        style={[
+          styles.container,
+          {backgroundColor: randomColors.randomBackground},
+        ]}>
+        <View style={[styles.box]}>
+          <View
+            style={[
+              styles.card,
+              styles.cardOne,
+              {backgroundColor: randomColors.randomBoxColor1},
+            ]}>
+            <Text />
+          </View>
+          <View
+            style={[
+              styles.card,
+              styles.cardTwo,
+              {backgroundColor: randomColors.randomBoxColor2},
+            ]}>
+            <Text />
+          </View>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+
+        <TouchableOpacity onPress={generateColor}>
+          <View style={styles.actionBtn}>
+            <Text style={styles.actionBtnTxt}>Press me</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ffffff',
   },
-  sectionTitle: {
+  actionBtn: {
+    borderRadius: 12,
+    backgroundColor: '#6A1B4D',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+  },
+  actionBtnTxt: {
     fontSize: 24,
-    fontWeight: '600',
+    color: '#FFFFFF',
+    textTransform: 'uppercase',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  card: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100,
+    height: 100,
+    borderRadius: 4,
+    margin: 8,
   },
-  highlight: {
-    fontWeight: '700',
+  cardOne: {
+    backgroundColor: '#EF5354',
+  },
+  cardTwo: {
+    backgroundColor: '#50DBB4',
+  },
+
+  box: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    width: '100%',
+    marginVertical: 20,
+    padding: 20,
   },
 });
 
--- bgchangerwithbox/App.tsx
+++ bgchangerwithbox/App.tsx
@@ -1,10 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React, {useState} from 'react';
 
 import {
@@ -16,11 +9,7 @@ import {
 } from 'react-native';
 
 function App(): JSX.Element {
-  const [randomColors, setRandomColors] = useState({
-    randomBackground: '#ffffff',
-    randomBoxColor1: '#EF5354',
-    randomBoxColor2: '#50DBB4',
-  });
+  const [randomBackground, setRandomBackground] = useState('#ffffff');
 
   const generateColor = () => {
     const hexRange = '0123456789ABCDEF';
@@ -29,66 +18,13 @@ function App(): JSX.Element {
     for (let i = 0; i < 6; i++) {
       color += hexRange[Math.floor(Math.random() * 16)];
     }
-
-    let randomBoxColor1 = '#';
-
-    for (let i = 0; i < 6; i++) {
-      randomBoxColor1 += hexRange[Math.floor(Math.random() * 16)];
-    }
-
-    // const randomBoxColor1 =
-    //   '#' +
-    //   hexRange[Math.floor(Math.random() * 16)] +
-    //   hexRange[Math.floor(Math.random() * 16)] +
-    //   hexRange[Math.floor(Math.random() * 16)];//this code only  but only uses 3 characters (representing RGB values) to
-    //create a 3-digit hexadecimal color code
-
-    let randomBoxColor2 = '#';
-
-    for (let i = 0; i < 6; i++) {
-      randomBoxColor2 += hexRange[Math.floor(Math.random() * 16)];
-    }
-
-    // const randomBoxColor2 =
-    //   '#' +
-    //   hexRange[Math.floor(Math.random() * 16)] +
-    //   hexRange[Math.floor(Math.random() * 16)] +
-    //   hexRange[Math.floor(Math.random() * 16)];
-
-    setRandomColors({
-      randomBackground: color,
-      randomBoxColor1,
-      randomBoxColor2,
-    });
+    setRandomBackground(color);
   };
 
   return (
     <>
-      <StatusBar backgroundColor={randomColors.randomBackground} />
-      <View
-        style={[
-          styles.container,
-          {backgroundColor: randomColors.randomBackground},
-        ]}>
-        <View style={[styles.box]}>
-          <View
-            style={[
-              styles.card,
-              styles.cardOne,
-              {backgroundColor: randomColors.randomBoxColor1},
-            ]}>
-            <Text />
-          </View>
-          <View
-            style={[
-              styles.card,
-              styles.cardTwo,
-              {backgroundColor: randomColors.randomBoxColor2},
-            ]}>
-            <Text />
-          </View>
-        </View>
-
+      <StatusBar backgroundColor={randomBackground} />
+      <View style={[styles.container, {backgroundColor: randomBackground}]}>
         <TouchableOpacity onPress={generateColor}>
           <View style={styles.actionBtn}>
             <Text style={styles.actionBtnTxt}>Press me</Text>
@@ -104,7 +40,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#ffffff',
   },
   actionBtn: {
     borderRadius: 12,
@@ -116,31 +51,6 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: '#FFFFFF',
     textTransform: 'uppercase',
-  },
-  card: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 100,
-    height: 100,
-    borderRadius: 4,
-    margin: 8,
-  },
-  cardOne: {
-    backgroundColor: '#EF5354',
-  },
-  cardTwo: {
-    backgroundColor: '#50DBB4',
-  },
-
-  box: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-    width: '100%',
-    marginVertical: 20,
-    padding: 20,
   },
 });
 
